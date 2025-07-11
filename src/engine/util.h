33d90bd62dbfc9e3c283e0983e21dc1204159665
lquinnMac <lewis.quinn.22@ucl.ac.uk>
2025-07-11 15:28:58 +0200

    Improved util defines to include more debugging information

--- src/engine/util.h
+++ src/engine/util.h
@@ -2,8 +2,9 @@
 
 #include <stdio.h>
 
-// error macros, source: JDH youtube
+// error and log macros, source: JDH youtube
 // do while used to ensure correct syntax in all use cases
+#define ERROR_EXIT(fmt, ...)      do {fprintf(stderr, "ERROR: %s:%d:%s(): " fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__); exit(-1);} while (0)
+#define ERROR_RETURN(R, fmt, ...) do {fprintf(stderr, "ERROR: %s:%d:%s(): " fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__); return R;} while (0)
+#define LOG_INFO(fmt, ...)        do {fprintf(stdout, "INFO: %s:%d:%s(): " fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__);} while (0)
 
-#define ERROR_EXIT(...)      do {fprintf(stderr, __VA_ARGS__); exit(-1);} while (0)
-#define ERROR_RETURN(R, ...) do {fprintf(stderr, __VA_ARGS__); return R;} while (0)
