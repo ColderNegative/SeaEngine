df25a424492ddc29e58409c79f681954432044a6
lquinnMac <lewis.quinn.22@ucl.ac.uk>
2025-07-11 15:30:41 +0200

    Added keyboard input polling with user callbacks for alphanumeric keys

---
+++ src/engine/input/keyboard.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "keyboard.h"
+#include "glfw3.h"
+#include "../util.h"
+#include "../render/window.h"
+
+
+Keyboard_Input *keyboard_create() {
+    Keyboard_Input *keyboard_input = malloc(sizeof(*keyboard_input));
+
+    *keyboard_input = (Keyboard_Input) {
+        .key_functions = calloc(SEAKEY_COUNT, sizeof(*keyboard_input->key_functions)),
+    };
+
+    return keyboard_input;
+}
+
+void keyboard_register_key(Keyboard_Input *keyboard_input, SEAKEY key, Key_Callback key_callback, void *context) {
+    keyboard_input->key_functions[key] = (Key_Function) {
+        .key_callback = key_callback,
+        .context = context,
+    };
+
+    LOG_INFO("info (keyboard): Key %d registered", key);
+
+    return;
+}
+
+void keyboard_deregister_key(Keyboard_Input *keyboard_input, SEAKEY key) {
+    keyboard_input->key_functions[key] = (Key_Function) {
+        .key_callback = NULL,
+        .context = NULL,
+    };
+
+    LOG_INFO("info (keyboard): Key %d deregistered", key);
+
+    return;
+}
+
+void keyboard_poll_keys(Keyboard_Input *keyboard_input, Window_State *window_state) {
+    for (int i = 0; i < SEAKEY_COUNT; i++) {
+        int glfw_key = 0;
+        if (i < 10) {
+            glfw_key = i + GLFW_NUMKEY_OFFSET;
+        } else {
+            glfw_key = i - SEAKEY_ALPHA_OFFSET + GLFW_ALPHAKEY_OFSSET;
+        }
+
+        SEAKEY_STATE state = glfwGetKey(window_state->window, glfw_key);
+        
+        if (keyboard_input->key_functions[i].key_callback != NULL) {
+            void *context = keyboard_input->key_functions[i].context;
+            keyboard_input->key_functions[i].key_callback(state, context);
+        }
+    }
+
+    return;
+}
+
+void keyboard_destroy(Keyboard_Input* keyboard_input) {
+    free(keyboard_input->key_functions);
+    free(keyboard_input);
+
+    return;
+}
+
---
+++ src/engine/input/keyboard.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#define SEAKEY_COUNT 36
+#define SEAKEY_ALPHA_OFFSET 10
+#define GLFW_NUMKEY_OFFSET 48
+#define GLFW_ALPHAKEY_OFSSET 65
+
+#include "../render/window.h"
+
+typedef enum {
+    SEAKEY_RELEASE,
+    SEAKEY_PRESS,
+} SEAKEY_STATE;
+
+typedef enum {
+    SEAKEY_0,
+    SEAKEY_1,
+    SEAKEY_2,
+    SEAKEY_3,
+    SEAKEY_4,
+    SEAKEY_5,
+    SEAKEY_6,
+    SEAKEY_7,
+    SEAKEY_8,
+    SEAKEY_9,
+    SEAKEY_A,
+    SEAKEY_B,
+    SEAKEY_C,
+    SEAKEY_D,
+    SEAKEY_E,
+    SEAKEY_F,
+    SEAKEY_G,
+    SEAKEY_H,
+    SEAKEY_I,
+    SEAKEY_J,
+    SEAKEY_K,
+    SEAKEY_L,
+    SEAKEY_M,
+    SEAKEY_N,
+    SEAKEY_O,
+    SEAKEY_P,
+    SEAKEY_Q,
+    SEAKEY_R,
+    SEAKEY_S,
+    SEAKEY_T,
+    SEAKEY_U,
+    SEAKEY_V,
+    SEAKEY_W,
+    SEAKEY_X,
+    SEAKEY_Y,
+    SEAKEY_Z,
+} SEAKEY;
+
+typedef void (*Key_Callback)(SEAKEY_STATE state, void *context);
+
+typedef struct {
+    Key_Callback key_callback;
+    void *context;
+} Key_Function;
+
+typedef struct {
+    Key_Function *key_functions;
+} Keyboard_Input;
+
+Keyboard_Input *keyboard_create();
+void keyboard_register_key(Keyboard_Input *keyboard_input, SEAKEY key, Key_Callback key_callback, void *context);
+void keyboard_deregister_key(Keyboard_Input *keyboard_input, SEAKEY key);
+void keyboard_poll_keys(Keyboard_Input *keyboard_input, Window_State *window_state);
+void keyboard_destroy(Keyboard_Input* keyboard_input);
